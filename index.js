333ab269dd02c5db57a2bc16465cb0c44753ce24
Clark Chen <yyunqingchenn@gmail.com>
2019-07-02 00:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key
-  return
+  delete object[key];
+  return object;
 }
